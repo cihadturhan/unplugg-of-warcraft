1fb26b97338c868716960640dd7bfcfeae0a2138
Nuno Silva <nunosilva800@gmail.com>
2017-08-04 09:09:07 -0300

    Improve logging and naming of vars; Improve validation

--- cmd/scraper/main.go
+++ cmd/scraper/main.go
@@ -10,6 +10,10 @@ import (
 	"time"
 )
 
+// which collection should we dump data into?
+const collectionForDump string = "auctions"
+const sleepTime time.Duration = 30 * time.Minute
+
 func main() {
 	// loads user flags.
 	var (
@@ -18,7 +22,7 @@ func main() {
 		apikey     = flag.String("apikey", "", "api authentication key")
 		mongoUrl   = flag.String("mongoUrl", "", "mongoDB url")
 		configPath = flag.String("config", ".env", "config file path")
-		debug      = flag.Bool("debug", false, "enable debug level logs")
+		debug      = flag.Bool("debug", true, "enable debug level logs")
 	)
 	flag.Parse()
 
@@ -33,79 +37,95 @@ func main() {
 		panic(err)
 	}
 
-	log.WithFields(log.Fields{"configPath": *configPath}).Info("starting crawler")
+	log.WithFields(log.Fields{"configPath": *configPath}).
+		Info("👀 starting crawler 👀")
 
-	lastDump := 0
+	// TODO: get last dump timestamp from DB or somthing
+	lastDumpTs := 0
 	for {
-		n, err := getDump(c, lastDump)
+		n, err := getDump(c, lastDumpTs)
 
 		if err == nil {
-			lastDump = n
+			lastDumpTs = n
 		}
-		time.Sleep(30 * time.Minute)
+
+		log.WithFields(log.Fields{"sleepTime": sleepTime}).
+			Info("so tired... going to rest a bit 😴")
+		time.Sleep(sleepTime)
 	}
 }
 
-// auctionIsValid cheks if an auction is valid
-func auctionIsValid(auction warcraft.Auction) bool {
-	if auction.Timeleft == "SHORT" {
-		return false
-	}
-
-	return true
-}
-
-// buildValidAuctionsSlice takes the auctions array and returns an slice with the valid auctions to be inserted into the DB
-func buildValidAuctionsSlice(allAuctions []warcraft.Auction) []interface{} {
-	validAuctions := make([]interface{}, 0)
-
-	for _, auction := range allAuctions {
-		if auctionIsValid(auction) {
-			validAuctions = append(validAuctions, auction)
-		}
-	}
-
-	return validAuctions
-}
-
-func getDump(c *warcraft.Config, last int) (int, error) {
+func getDump(cfg *warcraft.Config, previousTimestamp int) (int, error) {
 	// makes request to get dump url.
-	r, err := blizzard.NewRequest(c)
-
+	request, err := blizzard.NewRequest(cfg)
 	if err != nil {
 		return 0, err
 	}
+	timestamp := request.Modified
 
 	// dump already exists.
-	if r.Modified == last {
-		log.WithFields(log.Fields{"dump": last}).Warn("dump already exists")
-		return last, nil
+	if timestamp == previousTimestamp {
+		log.WithFields(log.Fields{"dump": previousTimestamp}).
+			Warn("dump already exists")
+		return previousTimestamp, nil
 	}
 
 	// gets the AH dump.
-	d, err := blizzard.NewDump(r.URL)
+	dump, err := blizzard.NewDump(request.URL)
 	if err != nil {
 		return 0, err
 	}
 
-	// open Mongo database
-	db, err := openDatabase(c.MongoUrl)
+	// get valid auctions from this dump
+	log.WithFields(log.Fields{"dump": timestamp}).
+		Info("validating auctions")
+	validAuctions := buildValidAuctionsSlice(dump.Auctions, timestamp)
 
+	// open Mongo database and store them
+	log.WithFields(
+		log.Fields{"dump": timestamp, "collection": collectionForDump},
+	).Info("storing into collection")
+	db, err := openDatabase(cfg.MongoUrl)
+	if err != nil {
+		return 0, err
+	}
+	err = db.C(collectionForDump).Insert(validAuctions...)
 	if err != nil {
 		return 0, err
 	}
 
-	// get collection and insert valid auctions into the database
-	collection := db.C("auctions")
-	validAuctions := buildValidAuctionsSlice(d.Auctions)
+	log.WithFields(log.Fields{"dump": timestamp}).Info("new dump created")
+	return timestamp, nil
+}
 
-	err = collection.Insert(validAuctions...)
-	if err != nil {
-		return 0, err
+// buildValidAuctionsSlice takes the auctions array and returns an array
+// with only the valid ones
+func buildValidAuctionsSlice(
+	allAuctions []warcraft.Auction, timestamp int,
+) []interface{} {
+	validAuctions := make([]interface{}, 0)
+	for _, auction := range allAuctions {
+		if auctionIsValid(auction) {
+			auction.Timestamp = timestamp
+			// TODO: we should check auction.ID to prevent duplicates from going into
+			// the database
+			validAuctions = append(validAuctions, auction)
+		}
 	}
+	return validAuctions
+}
 
-	log.WithFields(log.Fields{"dump": r.Modified}).Info("new dump created")
-	return r.Modified, nil
+// auctionIsValid checks if an auction is valid
+func auctionIsValid(auction warcraft.Auction) bool {
+	if auction.Timeleft == "SHORT" {
+		log.WithFields(log.Fields{"auction": auction}).Debug("invalid TimeLeft")
+		return false
+	}
+	if auction.Buyout == 0 {
+		log.WithFields(log.Fields{"auction": auction}).Debug("invalid Buyout")
+		return false
+	}
+	return true
 }
 
 // Connects to MongoDB, establishes a session and returns the database
@@ -114,12 +134,14 @@ func openDatabase(url string) (*mgo.Database, error) {
 	session, err := mgo.Dial(url)
 
 	if nil != err {
-		log.WithFields(log.Fields{"error": err, "url": url}).Error("failed to connect to MongoDB")
+		log.WithFields(log.Fields{"error": err, "url": url}).
+			Error("failed to connect to MongoDB")
 		return nil, err
 	}
 
 	session.EnsureSafe(&mgo.Safe{FSync: true, J: true})
-	log.WithFields(log.Fields{"database": config.MongoDBDatabase}).Info("Opening database")
+	log.WithFields(log.Fields{"database": config.MongoDBDatabase}).
+		Info("Opening database")
 
 	return session.DB(config.MongoDBDatabase), nil
 }
