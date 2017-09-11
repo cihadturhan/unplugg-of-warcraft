bdb6f0999d24c5e9a0a1062b3df8eded755bead1
Cihad Turhan <cihadturhan@gmail.com>
2017-09-11 15:55:30 +0300

    fonts

---
+++ landing/src/components/Timeline.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="timeline">
+    <div class="container">
+      <h2>SEE DETAILED TIMELINE OF ANY ITEMS</h2>
+      <div class="row justify-content-center">
+        <div class="col-md-10">
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rhoncus erat vitae felis luctus commodo. Morbi mattis aliquam turpis sed porta. Nullam vel imperdiet massa. Curabitur blandit ultricies volutpat. Morbi sit amet massa ut ligula rhoncus pharetra id in massa. Quisque et nunc elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        </div>
+      </div>
+
+      <div class="row justify-content-center mar-top-md">
+        <a class="btn btn-primary">Get Timeline of Your Items</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style>
+  .timeline{
+    width: 100vw;
+    height: 100vh;
+    text-align: center;
+    background-color: var(--color-light);
+    display: flex;
+    align-items: center;
+  }
+
+</style>
